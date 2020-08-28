93e02906d2afedbc347f60f9ec09efe69d6893a3
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2020-08-28 14:17:56 +0800

    Update and rename Main.js to SingleServer.js

--- SingleServer.js
+++ SingleServer.js
@@ -6,12 +6,12 @@ let Svr = {
 addEventListener(
   "fetch",
   (e) => {
-    let Url=new URL(e.request.url);
-    Url.host= Svr.Host;
+    let Url = new URL(e.request.url);
+    Url.host = Svr.Host;
     Url.port = Svr.Port;
     Url.protocol = Svr.Protocol + ":";
-    let Req=new Request(Url,e.request);
-    Req.headers.set('X-Real-Host',e.request.headers.get('Host'));
+    let Req = new Request(Url,e.request);
+    Req.headers.set('X-Real-Host', e.request.headers.get('Host'));
     e.respondWith(fetch(Req));
   }
 )
